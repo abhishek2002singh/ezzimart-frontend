00121ea030148a3b56698e7445ab348847c9e517
abhishek singh <singhsabhishek23@gmail.com>
2024-11-10 23:11:40 +0530

    make eccomerce website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Body from './components/Body';  // Main layout (Nav + Footer)
+import ProductDetails from './components/ProductDetails';  // Product detail page
+import About from './components/About';  // About page
+import Skills from './components/Skills';  // Skills page
+import Main from './components/Main';  // Main page
+import { Provider } from 'react-redux'
+import appStore from './utils/appStore'
+import CardItems from './components/cardItems';
+import Checkout from './components/Checkout';
+import Place from './components/Place';
+import Jobs from './components/Jobs'
+import Contact from './components/Contact'
+import Login from './components/Login';
+import Profile from './components/Profile';
+import EditProfile from './components/EditProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={appStore}>
+    <Router>
+      <Routes>
+       
+        <Route path="/" element={<Body />}>
+     
+          <Route index element={<Main />} /> 
+          <Route path="about" element={<About />} /> 
+          <Route path="cardItems" element={<CardItems />} /> 
+          <Route path="skills" element={<Skills />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="place" element={<Place />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="editProfile" element={<EditProfile />} />
+                
+
+         
+        <Route path="product/:id" element={<ProductDetails />} /> 
+        </Route> 
+      </Routes>
+    </Router>
+    </Provider>
   );
 }
 
